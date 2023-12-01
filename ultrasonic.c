b7fefc75a0d0f4196ef46972ce72b6949b2ea3f3
otavioassis-git <otavioassis2013@gmail.com>
2023-12-01 18:49:47 -0300

    creating ultrasonic sensor library

---
+++ ultrasonic.c
@@ -0,0 +1,25 @@
+#define OUTPUT 1
+#define INPUT 0
+
+#define HIGH 1
+#define LOW 0
+
+// Pinos sensor ultrassonico
+#define MISO_ECHO 19
+#define EN_TRIG 16
+
+void setupUltrassonicSensor() {
+    pinMode(MISO_ECHO, INPUT);
+    pinMode(EN_TRIG, OUTPUT);
+}
+
+float getDistance() {
+    digitalWrite(EN_TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(EN_TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(EN_TRIG, LOW);
+
+    int duration = pulseIn(MISO_ECHO, HIGH);
+    return duration / 58;
+}
