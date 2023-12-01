6bd4abfb5912a119aa8b18ae69389dd739d35b96
faakit <andrealtosan@gmail.com>
2023-12-01 12:23:38 -0300

    feat: lcd lib

---
+++ sketch_led_mqtt_generic.ino
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "lcd.h"
+
+// pinos de sensores
+#define SLC 36
+#define SL 35
+#define SR 39
+#define SC 34
+#define SRC 32
+
+void setup() {
+  setupLcd();
+}
+
+void loop() {
+  
+}
